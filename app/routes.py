e1504e7e783a662d612f41f8f244cea6b616fc98
sampauly <samuelpauly02@gmail.com>
2026-04-21 00:30:05 -0400

    add .env file, then run python run.py, should display Hello World and db connection should be successful

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from app.db import connect_db, check_db
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    check_db()
+    return "Hello World!"
